9bbff3dc9d32434f3f5eb168e6d7a152ddf662f4
ngxx-fus <msnp@outlook.com.vn>
2025-07-01 00:36:01 +0700

    changed

--- lua/ngxxfus/remap.lua
+++ lua/ngxxfus/remap.lua
@@ -1,1 +1,22 @@
+-- Move lines up/down with Alt + j/k or Alt + Up/Down arrows
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down", silent = true })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up", silent = true })
+vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==", { desc = "Move line down", silent = true })
+vim.keymap.set("n", "<A-Up>", ":m .-2<CR>==", { desc = "Move line up", silent = true })
+
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down", silent = true })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up", silent = true })
+vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down", silent = true })
+vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up", silent = true })
+
+
+-- Toggle comment with Ctrl+/
+-- Requires 'numToStr/Comment.nvim' plugin installed and set up
+vim.keymap.set("n", "<C-/>", function()
+  require("Comment.api").toggle.linewise.current()
+end, { desc = "Toggle comment (linewise)" })
+
+vim.keymap.set("v", "<C-/>", function()
+  require("Comment.api").toggle.linewise(vim.fn.visualmode())
+end, { desc = "Toggle comment (visual)", silent = true })
 
