71bc3cd7ede1c669d15bd3d87e113af963a3ebae
ngxx-fus <msnp@outlook.com.vn>
2025-07-11 09:38:29 +0700

    Initial commit

--- lua/ngxxfus/remap.lua
+++ lua/ngxxfus/remap.lua
@@ -9,6 +9,24 @@ vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up", s
 vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down", silent = true })
 vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up", silent = true })
 
+<<<<<<< HEAD
+=======
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move line(s) down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move line(s) up" })
+
+-- Tab navigation using <leader>
+vim.keymap.set("n", "<leader><Right>" , ":bnext<CR>"      , { desc = "Next buffer", silent = true })
+vim.keymap.set("n", "<leader><Left>"  , ":bprevious<CR>"  , { desc = "Previous buffer", silent = true })
+vim.keymap.set("n", "<leader>h"       , ":bnext<CR>"      , { desc = "Next tab", silent = true })
+vim.keymap.set("n", "<leader>l"       , ":bprevious<CR>"  , { desc = "Previous tab", silent = true })
+vim.keymap.set("n", "<leader>n"       , ":tabnew<CR>"       , { desc = "Previous tab", silent = true })
+vim.keymap.set("n", "<leader>H"       , "<Cmd>BufferLineMovePrev<CR>", { desc = "Move tab left" })
+vim.keymap.set("n", "<leader>L"       , "<Cmd>BufferLineMoveNext<CR>", { desc = "Move tab right" })
+
+-- Terminal
+vim.keymap.set("t", "<C-\\>", "<C-\\><C-n>", { desc = "Terminal: exit to normal mode" })
+
+>>>>>>> ab04854 (changed)
 
 -- Toggle comment with Ctrl+/
 -- Requires 'numToStr/Comment.nvim' plugin installed and set up
@@ -20,3 +38,24 @@ vim.keymap.set("v", "<C-/>", function()
   require("Comment.api").toggle.linewise(vim.fn.visualmode())
 end, { desc = "Toggle comment (visual)", silent = true })
 
+<<<<<<< HEAD
+=======
+-- New Terminal at the bottom
+vim.keymap.set("n", "<leader>t", function()
+  -- Get total height of current window
+  local total_height = vim.api.nvim_win_get_height(0)
+  local split_height = math.floor(total_height / 4)
+
+  -- Create horizontal split and resize it
+  vim.cmd("belowright split")
+  vim.cmd(split_height .. "wincmd _")  -- Set height of the split
+  vim.cmd("terminal")
+  vim.cmd("startinsert")
+end, { desc = "Open horizontal split (1/4 height)", silent = false })
+
+
+
+-- Search
+vim.keymap.set("v", "//", [[y/\V<C-r>=escape(@",'/\')<CR><CR>]], { noremap = true, silent = true, desc = "Search for selected text" })
+
+>>>>>>> ab04854 (changed)
--- lua/ngxxfus/set.lua
+++ lua/ngxxfus/set.lua
@@ -7,10 +7,17 @@ vim.opt.clipboard = 'unnamedplus' -- use system keyboard for yank
 vim.opt.nu = true                 -- set line numbers -- set line numbers
 vim.opt.relativenumber = true     -- use relative line numbers
  
+<<<<<<< HEAD
 -- set tab size to 2 spaces
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+=======
+-- set tab size to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+>>>>>>> ab04854 (changed)
 vim.opt.expandtab = true
 vim.opt.smartindent = true
  
